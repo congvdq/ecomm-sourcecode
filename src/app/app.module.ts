e2925673dfe1e76d911ae62dc91cdf6e6890d424
congvdq <congvdq@hotmail.com>
2018-05-21 19:51:29 +0700

    generate component footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ListproductComponent } from './listproduct/listproduct.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
@@ -13,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     ListproductComponent,
     HeaderComponent,
+    FooterComponent,
     
   ],
   imports: [
